75b318750410cf63df6c84ab816517c074cde2ed
iabsensimaster <iabsensimaster@gmail.com>
2025-10-02 12:24:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxyW07kNot1GMBUi_l8aGEQZFgk8tQJUXsLuR2LCOd2i8E9u-bhPqPKKR3M0tyFpbj29g/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxPG5ubjTtk_4PFf9V2mxFshORdhlWHYytt3xKbg8KxL-E9aJw65vtRg1PvQtXsaIQY/exec";
 
 function getAPIUrl(action) {
   const id = localStorage.getItem("id_spreadsheet");
